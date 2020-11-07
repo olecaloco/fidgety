a6c09b5f51eab5682b6419947c7b7c94c2053776
olecaloco <jedmacapagal@gmail.com>
2020-11-07 15:27:42 +0000

    Deploying to gh-pages from  @ 4f4fc186321ff4a4d992a708b9140e5ec7dede25 🚀

---
+++ precache-manifest.53c8053729bde6d9d6b36eafef8e15e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc83bf8f74d12f45a0bf",
+    "url": "/css/app.604adff2.css"
+  },
+  {
+    "revision": "1d2ddd1dba523b02a85da96ff0fd68f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc83bf8f74d12f45a0bf",
+    "url": "/js/app.6ec5a10d.js"
+  },
+  {
+    "revision": "ec28e5f878e397aa37f1",
+    "url": "/js/chunk-vendors.f2f7a37f.js"
+  },
+  {
+    "revision": "ba27355b95037bde3b1abe89e9089e70",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
