b96668686e9f0f2f3ed3ff84fc04118b202e7120
olecaloco <jedmacapagal@gmail.com>
2020-11-08 06:35:28 +0000

    Deploying to gh-pages from  @ ee0f459adf58f7d94d767358149528b645ea5a2b 🚀

---
+++ precache-manifest.7442f92ba453a77f1536d4bfbbe96350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e484abff3048f9b1d343",
+    "url": "css/app.604adff2.css"
+  },
+  {
+    "revision": "f98c81f5746062e1c7968f03678d841d",
+    "url": "index.html"
+  },
+  {
+    "revision": "e484abff3048f9b1d343",
+    "url": "js/app.29f20b2c.js"
+  },
+  {
+    "revision": "ec28e5f878e397aa37f1",
+    "url": "js/chunk-vendors.f2f7a37f.js"
+  },
+  {
+    "revision": "ba27355b95037bde3b1abe89e9089e70",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.85575736347789ee327f387750d98774.js"
+  "precache-manifest.7442f92ba453a77f1536d4bfbbe96350.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fidgety-vue"});
